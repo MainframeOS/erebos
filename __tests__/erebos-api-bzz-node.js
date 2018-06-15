dd35ae1141d04a22db8c8261f31642e2a71cb885
Miloš Mošić <mosic.milos@gmail.com>
2018-06-15 18:54:50 +0200

    Add integration tests using a local Swarm instance

---
+++ __tests__/erebos-api-bzz-node.js
@@ -0,0 +1,54 @@
+import Bzz from '../packages/erebos-api-bzz-node'
+import fetch from 'node-fetch'
+
+describe('Bzz', () => {
+  const url = 'http://localhost:8500'
+  const bzz = new Bzz(url)
+  bzz._fetch = require('node-fetch')
+
+  it('trying to upload without specifying content-type fails', async () => {
+    const uploadRequest = bzz.upload('hello')
+    expect(uploadRequest).rejects.toThrow('Bad Request')
+  })
+
+  it('uploading and downloading single file using bzz', async () => {
+    const uploadContent = 'hello'
+    const headers = {'Content-Type': 'text/plain'}
+    const manifestHash = await bzz.upload('hello', headers)
+
+    const response = await bzz.download(manifestHash)
+    expect(await response.text()).toBe(uploadContent)
+
+    const text = await bzz.downloadText(manifestHash)
+    expect(text).toBe(uploadContent)
+
+    const buffer = await bzz.downloadBuffer(manifestHash)
+    expect(buffer.toString('utf8')).toBe(uploadContent)
+  })
+
+  it('uploading and downloading single file using bzz-raw', async () => {
+    const uploadContent = 'hello'
+    const hash = await bzz.uploadRaw('hello')
+
+    const response = await bzz.downloadRaw(hash)
+    expect(await response.text()).toBe(uploadContent)
+
+    const text = await bzz.downloadRawText(hash)
+    expect(text).toBe(uploadContent)
+
+    const buffer = await bzz.downloadRawBuffer(hash)
+    expect(buffer.toString('utf8')).toBe(uploadContent)
+  })
+
+  it('downloading the manifest', async () => {
+    const uploadContent = 'hello'
+    const headers = {'Content-Type': 'text/plain'}
+    const manifestHash = await bzz.upload('hello', headers)
+
+    // Requesting manifestHash with bzz-raw directly returns the manifest file
+    const manifest = await bzz.downloadRawText(manifestHash)
+    const entry = JSON.parse(manifest).entries[0]
+    expect(entry.contentType).toBe('text/plain')
+    expect(entry.size).toBe(uploadContent.length)
+  })
+})
