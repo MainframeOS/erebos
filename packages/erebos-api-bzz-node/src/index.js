edbefb80148fcd065426112e866073d6d6abb724
Miloš Mošić <mosic.milos@gmail.com>
2018-06-15 18:51:41 +0200

    Add downloadBuffer function to Bzz

--- packages/erebos-api-bzz-node/src/index.js
+++ packages/erebos-api-bzz-node/src/index.js
@@ -9,6 +9,10 @@ export default class Bzz extends BaseBzz {
     this._fetch = fetch
   }
 
+  downloadBuffer(hash: string): Promise<Buffer> {
+    return this.download(hash).then(res => res.buffer())
+  }
+
   downloadRawBuffer(hash: string): Promise<Buffer> {
     return this.downloadRaw(hash).then(res => res.buffer())
   }
