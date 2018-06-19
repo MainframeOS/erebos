688c1f7cab1b05594dc54fdb8d53981c9c6a1fde
Miloš Mošić <mosic.milos@gmail.com>
2018-06-19 17:19:58 +0200

    Update downloadBuffer function to support providing content path

--- packages/erebos-api-bzz-node/src/index.js
+++ packages/erebos-api-bzz-node/src/index.js
@@ -9,8 +9,8 @@ export default class Bzz extends BaseBzz {
     this._fetch = fetch
   }
 
-  downloadBuffer(hash: string): Promise<Buffer> {
-    return this.download(hash).then(res => res.buffer())
+  downloadBuffer(hash: string, path?: string = ''): Promise<Buffer> {
+    return this.download(hash, path).then(res => res.buffer())
   }
 
   downloadRawBuffer(hash: string): Promise<Buffer> {
