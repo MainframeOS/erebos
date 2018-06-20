28841f2f43332f74fe3b6df84fcf23d629cf4b7c
Miloš Mošić <mosic.milos@gmail.com>
2018-06-20 13:42:31 +0200

    Simplify swarm script to only start swarm, not build binaries

--- start_swarm_node.sh
+++ start_swarm_node.sh
@@ -5,32 +5,12 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-GIT_TAG="swarm-network-rewrite"
 DATADIR="$1"
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-EXT_DEPS_DIR="$SCRIPTPATH/external_deps"
-GODIR="$EXT_DEPS_DIR/go-ethereum"
-
-mkdir -p "$EXT_DEPS_DIR"
-
-if [[ ! -e $GODIR ]]; then
-    echo "cloning the go-ethereum repo"
-    cd "$EXT_DEPS_DIR"
-    git clone --depth 1 https://github.com/MainframeHQ/go-ethereum.git -b $GIT_TAG
-fi
-
-cd "$GODIR"
-# doing the fetch here and now makes sure that we can change the chosen
-# commit hash without the risk of breaking the script
-git fetch --depth 1 origin $GIT_TAG
-git checkout $GIT_TAG
-make geth
-make swarm
 
 if [[ ! -e $DATADIR/keystore ]]; then
-  mkdir -p $DATADIR
-  echo 'secret' > $DATADIR/password
-  $GODIR/build/bin/geth --datadir $DATADIR account new --password $DATADIR/password
+    mkdir -p $DATADIR
+    echo 'secret' > $DATADIR/password
+    /app/bin/geth --datadir $DATADIR account new --password $DATADIR/password
 fi
 
 which jq
@@ -42,7 +22,7 @@ else
     exit 1
 fi
 
-$GODIR/build/bin/swarm \
+/app/bin/swarm \
     --datadir $DATADIR \
     --password $DATADIR/password \
     --verbosity 4 \
