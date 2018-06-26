27a31169af8b282e89cb51b7241aa7dc6a699890
Miloš Mošić <mosic.milos@gmail.com>
2018-06-26 17:37:04 +0200

    Add test setup for erebos-api-bzz-browser, with a passing TODO test

---
+++ __tests__/erebos-api-bzz-browser.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Bzz from '../packages/erebos-api-bzz-browser'
+
+describe('Bzz', () => {
+  let uploadContent
+  const url = 'http://localhost:8500/'
+  const bzz = new Bzz(url)
+
+  beforeEach(() => {
+    uploadContent = Math.random().toString(36).slice(2)
+  })
+
+  it('uploadDirectory() uploads the contents and returns the hash of the manifest', async () => {
+    const dir = {
+      "foo.txt": {data: "this is foo.txt"},
+      "bar.txt": {data: "this is bar.txt"}
+    }
+    const uploadRequest = bzz.uploadDirectory(dir)
+    expect(uploadRequest).rejects.toThrow('Not Implemented');
+  })
+})
