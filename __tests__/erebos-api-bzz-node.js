ba7738cc1f44e213199d95c1583b7beec9490d1f
Miloš Mošić <mosic.milos@gmail.com>
2018-07-31 17:43:03 +0200

    Wrap test file writes in Promise.all()

--- __tests__/erebos-api-bzz-node.js
+++ __tests__/erebos-api-bzz-node.js
@@ -184,14 +184,16 @@ describe('bzz-node', () => {
 
     const tempDirPath = path.join(os.tmpdir(), 'erebos-test-temp')
     await fs.ensureDir(tempDirPath)
-    await fs.outputFile(
-      path.join(tempDirPath, 'foo.txt'),
-      `this is foo.txt - ${uploadContent}`,
-    )
-    await fs.outputFile(
-      path.join(tempDirPath, 'bar.txt'),
-      `this is bar.txt - ${uploadContent}`,
-    )
+    Promise.all([
+      fs.outputFile(
+        path.join(tempDirPath, 'foo.txt'),
+        `this is foo.txt - ${uploadContent}`,
+      ),
+      fs.outputFile(
+        path.join(tempDirPath, 'bar.txt'),
+        `this is bar.txt - ${uploadContent}`,
+      ),
+    ])
 
     const dirHash = await bzz.uploadDirectoryTar(tempDirPath)
     const response = await bzz.downloadDirectoryData(dirHash)
