b769eba1296513cb54aff14ffd7fcb9bd2b13969
Miloš Mošić <milos@mainframe.com>
2018-08-08 16:21:09 +0200

    Use live requests to swarm in tests for the browser

--- __tests__/erebos-api-bzz-browser.js
+++ __tests__/erebos-api-bzz-browser.js
@@ -2,15 +2,23 @@
  * @jest-environment jsdom
  */
 
+import fetch from 'node-fetch'
 import Bzz from '../packages/erebos-api-bzz-browser'
 
 describe('bzz-browser', () => {
   const url = 'http://localhost:8500/'
   const bzz = new Bzz(url)
+  bzz._fetch = fetch // Injected by extending class
 
-  it('uploadDirectory() uploads the contents and returns the hash of the manifest', async () => {
+  it('uploadDirectory() is not implemented for the browser yet', async () => {
     const dir = {}
     const uploadRequest = bzz.uploadDirectory(dir)
     expect(uploadRequest).rejects.toThrow('Not Implemented')
+  })
+
+  it('trying to download non-existent hash raises an error', async () => {
+    const hash = 'abcdef123456'
+    const downloadRequest = bzz.downloadRawBlob(hash)
+    expect(downloadRequest).rejects.toThrow('Not Found')
   })
 })
