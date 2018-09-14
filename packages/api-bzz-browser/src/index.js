e99c70618be1adbd5d10675c06c78accc25bb9ad
Paul Le Cam <paul@ulem.net>
2018-09-14 17:48:49 +0100

    Bzz APIs refactoring (#42)
    * Start refactoring Bzz methods
    
    * Update tests for Bzz APIs refactoring
    
    * Fix support for defaultPath option in tar upload and add relevant tests
    
    * Update to latest master

--- packages/api-bzz-browser/src/index.js
+++ packages/api-bzz-browser/src/index.js
@@ -2,18 +2,20 @@
 /* eslint-env browser */
 
 import BaseBzz, {
+  resText,
   type DirectoryData, // eslint-disable-line import/named
 } from '@erebos/api-bzz-base'
+
+export type * from '@erebos/api-bzz-base'
 
 export default class Bzz extends BaseBzz {
   constructor(url: string) {
     super(url)
     this._fetch = window.fetch.bind(window)
-    this._FormData = window.FormData.bind(window)
   }
 
   uploadDirectory(directory: DirectoryData): Promise<string> {
-    const form = new this._FormData()
+    const form = new FormData()
     Object.keys(directory).forEach(function(key) {
       form.append(
         key,
@@ -24,14 +26,6 @@ export default class Bzz extends BaseBzz {
     return this._fetch(`${this._url}bzz:/`, {
       method: 'POST',
       body: form,
-    }).then(
-      res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))),
-    )
-  }
-
-  downloadRawBlob(hash: string): Promise<Blob> {
-    return this.downloadRaw(hash).then(
-      res => (res.ok ? res.blob() : Promise.reject(new Error(res.statusText))),
-    )
+    }).then(resText)
   }
 }
--- packages/api-bzz-node/src/index.js
+++ packages/api-bzz-node/src/index.js
@@ -1,156 +1,101 @@
 // @flow
 
-import path from 'path'
-import fs from 'fs'
 import type { Readable } from 'stream'
 import BaseBzz, {
-  type DirectoryData, // eslint-disable-line import/named
-  type FileEntry, // eslint-disable-line import/named
+  type DirectoryData,
+  type FileEntry,
+  type DownloadOptions,
+  type UploadOptions,
 } from '@erebos/api-bzz-base'
 import FormData from 'form-data'
-import { createWriteStream, ensureDir } from 'fs-extra'
+import { createReadStream } from 'fs-extra'
 import fetch from 'node-fetch'
-import tarFs from 'tar-fs'
-import tarStream from 'tar-stream'
 import { Observable } from 'rxjs'
+import tarStream from 'tar-stream'
 
-export const writeStreamTo = async (
-  stream: Readable,
-  filePath: string,
-): Promise<void> => {
-  await ensureDir(path.dirname(filePath))
-  await new Promise((resolve, reject) => {
-    stream
-      .pipe(createWriteStream(filePath))
-      .on('error', err => {
-        reject(err)
-      })
-      .on('finish', () => {
-        resolve()
-      })
-  })
-}
+import { isFile, writeStreamTo, extractTarStreamTo, packTar } from './fs'
 
-export const extractTarStreamTo = (
-  stream: Readable,
-  dirPath: string,
-): Promise<number> => {
-  return new Promise((resolve, reject) => {
-    const extract = tarStream.extract()
-    const writeFiles = [] // Keep track of files to write
-    extract.on('entry', (header, stream, next) => {
-      if (header.type === 'file' && header.name.length > 0) {
-        const filePath = path.join(dirPath, header.name)
-        const fileWritten = writeStreamTo(stream, filePath).then(() => {
-          next() // Extract next entry after file has been written
-        })
-        writeFiles.push(fileWritten)
-      } else {
-        next()
-      }
-    })
-    extract.on('error', err => {
-      reject(err)
-    })
-    extract.on('finish', () => {
-      // Wait until all files have been written before resolving
-      Promise.all(writeFiles).then(
-        () => {
-          resolve(writeFiles.length)
-        },
-        err => {
-          reject(err)
-        },
-      )
-    })
-    ensureDir(dirPath).then(
-      () => {
-        stream.pipe(extract)
-      },
-      err => {
-        reject(err)
-      },
-    )
-  })
-}
+export type * from '@erebos/api-bzz-base'
 
 export default class Bzz extends BaseBzz {
   constructor(url: string) {
     super(url)
     this._fetch = fetch
-    this._FormData = FormData
   }
 
-  uploadDirectory(directory: DirectoryData): Promise<string> {
-    const form = new this._FormData()
-    Object.keys(directory).forEach(function(key) {
+  async uploadDirectory(
+    directory: DirectoryData,
+    options?: UploadOptions = {},
+  ): Promise<string> {
+    const form = new FormData()
+    Object.keys(directory).forEach(key => {
       form.append(key, directory[key].data, {
         contentType: directory[key].contentType,
       })
     })
-
-    return this._fetch(`${this._url}bzz:/`, {
-      method: 'POST',
-      body: form,
-      headers: form.getHeaders(),
-    }).then(
-      res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))),
-    )
+    if (options.defaultPath != null) {
+      const file = directory[options.defaultPath]
+      if (file != null) {
+        form.append('', file.data, { contentType: file.contentType })
+      }
+    }
+    return await this._upload(form, options, form.getHeaders())
   }
 
-  uploadTarData(directory: DirectoryData): Promise<string> {
-    const pack = tarStream.pack()
-    Object.keys(directory).forEach(function(key) {
-      pack.entry({ name: key }, directory[key].data)
+  async _uploadTarStream(
+    stream: Readable,
+    options?: UploadOptions = {},
+  ): Promise<string> {
+    return await this._upload(stream, options, {
+      'content-type': 'application/x-tar',
     })
-    pack.finalize()
-
-    return this._fetch(`${this._url}bzz:/`, {
-      method: 'POST',
-      body: pack,
-      headers: {
-        'Content-Type': 'application/x-tar',
-      },
-    }).then(
-      res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))),
-    )
   }
 
-  uploadTarFile(path: string): Promise<string> {
-    return this._fetch(`${this._url}bzz:/`, {
-      method: 'POST',
-      body: fs.createReadStream(path),
-      headers: {
-        'Content-Type': 'application/x-tar',
-      },
-    }).then(
-      res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))),
-    )
+  // path must be either a tar archive or a directory
+  async uploadTar(path: string, options?: UploadOptions = {}): Promise<string> {
+    const stream = (await isFile(path))
+      ? createReadStream(path)
+      : packTar(path, options)
+    return await this._uploadTarStream(stream, options)
   }
 
-  uploadDirectoryFrom(path: string): Promise<string> {
-    return this._fetch(`${this._url}bzz:/`, {
-      method: 'POST',
-      body: tarFs.pack(path),
-      headers: {
-        'Content-Type': 'application/x-tar',
-      },
-    }).then(
-      res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))),
-    )
+  async uploadDirectoryFrom(
+    path: string,
+    options?: UploadOptions = {},
+  ): Promise<string> {
+    return await this._uploadTarStream(packTar(path, options), options)
   }
 
-  downloadDirectoryTar(hash: string): Promise<*> {
-    return this._fetch(`${this._url}bzz:/${hash}`, {
-      headers: {
-        Accept: 'application/x-tar',
-      },
-    }).then(res => (res.ok ? res : Promise.reject(new Error(res.statusText))))
+  async uploadFileFrom(
+    path: string,
+    options?: UploadOptions = {},
+  ): Promise<string> {
+    const raw = options.contentType == null
+    const headers = raw ? {} : { 'content-type': options.contentType }
+    return await this._upload(createReadStream(path), options, headers, raw)
   }
 
-  downloadDirectoryObservable(hash: string): Observable<FileEntry> {
+  async uploadFrom(
+    path: string,
+    options?: UploadOptions = {},
+  ): Promise<string> {
+    if (await isFile(path)) {
+      return await this.uploadFileFrom(path, options)
+    } else {
+      return await this.uploadDirectoryFrom(path, options)
+    }
+  }
+
+  async _downloadTar(hash: string, options: DownloadOptions): Promise<*> {
+    return await this._download(hash, options, { accept: 'application/x-tar' })
+  }
+
+  downloadObservable(
+    hash: string,
+    options?: DownloadOptions = {},
+  ): Observable<FileEntry> {
     return Observable.create(observer => {
-      this.downloadDirectoryTar(hash).then(
+      this._downloadTar(hash, options).then(
         res => {
           const extract = tarStream.extract()
           extract.on('entry', (header, stream, next) => {
@@ -184,10 +129,13 @@ export default class Bzz extends BaseBzz {
     })
   }
 
-  downloadDirectoryData(hash: string): Promise<DirectoryData> {
+  downloadDirectoryData(
+    hash: string,
+    options?: DownloadOptions = {},
+  ): Promise<DirectoryData> {
     return new Promise((resolve, reject) => {
       const directoryData = {}
-      this.downloadDirectoryObservable(hash).subscribe({
+      this.downloadObservable(hash, options).subscribe({
         next: entry => {
           directoryData[entry.path] = { data: entry.data, size: entry.size }
         },
@@ -201,17 +149,33 @@ export default class Bzz extends BaseBzz {
     })
   }
 
-  downloadDirectoryTo(hash: string, dirPath: string): Promise<number> {
-    return this.downloadDirectoryTar(hash).then(res => {
-      return extractTarStreamTo(res.body, dirPath)
-    })
+  async downloadDirectoryTo(
+    hash: string,
+    toPath: string,
+    options?: DownloadOptions = {},
+  ): Promise<number> {
+    const res = await this._downloadTar(hash, options)
+    return await extractTarStreamTo(res.body, toPath)
   }
 
-  downloadBuffer(hash: string, path?: string = ''): Promise<Buffer> {
-    return this.download(hash, path).then(res => res.buffer())
+  async downloadFileTo(
+    hash: string,
+    toPath: string,
+    options?: DownloadOptions = {},
+  ): Promise<void> {
+    const res = await this._download(hash, options)
+    await writeStreamTo(res.body, toPath)
   }
 
-  downloadRawBuffer(hash: string): Promise<Buffer> {
-    return this.downloadRaw(hash).then(res => res.buffer())
+  async downloadTo(
+    hash: string,
+    toPath: string,
+    options?: DownloadOptions = {},
+  ): Promise<void> {
+    if (await isFile(toPath)) {
+      await this.downloadFileTo(hash, toPath, options)
+    } else {
+      await this.downloadDirectoryTo(hash, toPath, options)
+    }
   }
 }
