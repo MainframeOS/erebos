a6e579e24ddb603a99e866b6a24b127a8469fcf0
Miloš Mošić <milos@mainframe.com>
2019-05-07 18:03:17 +0200

    Disable Swarm node syncing

--- start_swarm_node.sh
+++ start_swarm_node.sh
@@ -7,6 +7,7 @@ docker run \
        --volume /tmp/swarm-data:/data \
        --interactive \
        --tty "ethdevops/swarm:v0.3.11" \
+       --nosync \
        --maxpeers=0 \
        --verbosity=4 \
        --httpaddr=0.0.0.0 \
