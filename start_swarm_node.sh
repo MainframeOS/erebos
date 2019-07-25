2242fe96e446473cc256c1ed4d8bd986a04988ff
Paul Le Cam <paul@mainframe.com>
2019-07-25 15:26:22 +0100

    Run tests with Swarm v0.4.3

--- start_swarm_node.sh
+++ start_swarm_node.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
+mkdir -p /tmp/swarm/data && \
+echo "password" > /tmp/swarm/password && \
 docker run \
-       --publish 8500:8500 \
-       --publish 8546:8546 \
-       --env DATADIR=/data \
-       --env PASSWORD=password123 \
-       --volume /tmp/swarm-data:/data \
-       --interactive \
-       --tty \
-       "ethersphere/swarm:0.4.1" \
-       --nosync \
-       --maxpeers=0 \
-       --verbosity=4 \
-       --httpaddr=0.0.0.0 \
-       --nat=none \
-       --corsdomain="*" \
-       --ws \
-       --wsaddr=0.0.0.0 \
-       --wsorigins="*"
+  --publish 8500:8500 \
+  --publish 8546:8546 \
+  --volume /tmp/swarm:/swarm \
+  --interactive \
+  --tty \
+  "ethersphere/swarm:0.4.3" \
+    --datadir /swarm/data \
+    --password /swarm/password \
+    --nosync \
+    --maxpeers=0 \
+    --verbosity=4 \
+    --httpaddr=0.0.0.0 \
+    --nat=none \
+    --corsdomain="*" \
+    --ws \
+    --wsaddr=0.0.0.0 \
+    --wsorigins="*"
