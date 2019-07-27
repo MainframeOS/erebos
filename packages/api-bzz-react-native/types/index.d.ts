0357c7790b3235047ebc5e3b02472c1083ef2b47
Mark Vujevits <mark.vujevits@prezi.com>
2019-07-27 15:25:28 +0200

    fix commands in package.json

--- packages/api-bzz-react-native/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import BaseBzz, { DirectoryData, UploadOptions } from '@erebos/api-bzz-base'
-import { hexValue } from '@erebos/hex'
-
-export default class Bzz extends BaseBzz<Response> {
-  uploadDirectory(
-    directory: DirectoryData,
-    options?: UploadOptions,
-  ): Promise<hexValue>
-}
